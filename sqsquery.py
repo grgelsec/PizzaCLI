1723336b1b2e06ddef8fb284be9dec818c4a61f6
grgelsec <georgelisec@gmail.com>
2024-07-25 14:50:25 -0400

    env vars established securely

--- sqsquery.py
+++ sqsquery.py
@@ -1,13 +1,17 @@
 import boto3
 from botocore.exceptions import ClientError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 sqs = boto3.client('sqs',
-    aws_access_key_id='take',
-    aws_secret_access_key='the',
+    aws_access_key_id = os.getenv('SQS_ACCESS_KEY'),
+    aws_secret_access_key = os.getenv('SQS_SECRET_ACCESS_KEY'),
     region_name='us-east-2'
 )
 
-queue_url = 'L'
+queue_url = os.getenv('SQS_URL')
 
 def get_order(max_number, wait_time):
     response = sqs.receive_message(
@@ -19,6 +23,7 @@ def get_order(max_number, wait_time):
     )
     
     messages = response.get('Messages', [])
+    #need to add proper error handling for failed requests
     if messages:
         message = messages[1]
         order = message['Body']
