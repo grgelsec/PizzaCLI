81b31a8154f52fc8186deebce8468a85d738c555
grgelsec <georgelisec@gmail.com>
2024-07-31 14:54:53 -0400

    done w this thang

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import click
 import sqsquery
 import time 
+import asyncio
+import random
 
 #grab order from sqs and assign to a chef, assign to chef one, if chef one is busy, assign to chef two, if both chefs are busy, add to waitlist 
     
@@ -16,57 +18,62 @@ def print_banner():
 
 def setTime(order):
     time_dict = {
-        'Pizza': 10,
+        'Pizza': 5,
         'Wings': 3,
-        'Pasta': 8,
+        'Pasta': 4,
         'Lasagna': 5,
-        'Calzone': 12,
-        'Cheese Pizza': 7,
-        'Pepperoni Pizza': 13,
-        'Vodka Pasta': 14,
-        'Breadsticks': 6,
+        'Calzone': 4,
+        'Cheese Pizza': 2,
+        'Pepperoni Pizza': 3,
+        'Vodka Pasta': 1,
+        'Breadsticks': 2,
         'Pretzel Sticks': 4
     }
     return time_dict.get(order, 0)
 
-@click.group()
-def cli():
-    pass
+async def cook_order(chefs_name, order):
+    cook_time = setTime(order)
+    print(f"{chefs_name} is cooking {order} for {cook_time} seconds")
+    await asyncio.sleep(cook_time)
+    print(f"{chefs_name} finished cooking {order}")
+
+async def chef(name, order_queue):
+    while True:
+        if order_queue:
+            order = order_queue.pop(0)
+            await cook_order(name, order)
+        else:
+            await asyncio.sleep(1)
+
 
 @click.command()
 def start():
     click.echo(print_banner())
     click.echo("Welcome to George's Pizza Shop!")
 
-@click.command()
+
 #calls get_orders form sqsquery and forms a order list
-#could put the timers in the loop so that they are dependent on the timers finishing to pop orders form the array
-def cook():
-    customer_Orders = []
-    for x in range(5):
+async def cook():
+    customer_orders = []
+    random_amount = random.randint(1, 5)
+    for x in range(random_amount):
         print(f"Getting order {x + 1}")
-        order = sqsquery.get_order(5)
-        customer_Orders.append(order)
-    while len(customer_Orders) != 0:
-        order_time1 = setTime(customer_Orders[0])
-        customer_Orders.pop(0)
-        if len(customer_Orders) > 1:
-            chefTwo = True
-            order_time2 = setTime(customer_Orders[1])
-            customer_Orders.pop(1)
-        print("Chef one is cooking")
-        time.sleep(order_time1)
-        if chefTwo:
-            time.sleep(order_time2)
-            print("Chef two is cooking")
-        print("ready to serve")
-    print(customer_Orders)
+        order = sqsquery.get_order(random_amount)
+        customer_orders.append(order)
+    print(f"Orders received: {customer_orders}")
+    chef1 = asyncio.create_task(chef("Chef 1", customer_orders))
+    chef2 = asyncio.create_task(chef("Chef 2", customer_orders))
+    await asyncio.gather(chef1, chef2)
+    return print("Closing Shop")
     
-
-
-cli.add_command(start)
-cli.add_command(cook)
-
+            
+def run_cook():
+    click.echo(print_banner())
+    asyncio.run(cook())
+        
+@click.command()
+def cli():
+    run_cook()
 
 if __name__ == '__main__':
     cli()
